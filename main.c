a1d77c2b2ac609286d64d391666e81335b463712
= <dustin.alan.briscoe@gmail.com>
2025-07-12 12:05:25 -0400

    Adding curl and jansson libraries

--- main.c
+++ main.c
@@ -1,6 +1,79 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <jansson.h>
 
-int main(char* args) {
+struct MemoryStruct {
+    char *memory;
+    size_t size;
+};
 
-    
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (!ptr) {
+        fprintf(stderr, "Not enough memory!\n");
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
+int main(void) {
+    CURL *curl;
+    CURLcode res;
+
+    struct MemoryStruct chunk;
+    chunk.memory = malloc(1);
+    chunk.size = 0;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        const char *json_data = "{\"model\":\"qwen:8b\",\"prompt\":\"What is the capital of France?\",\"stream\":false}";
+
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:11434/api/generate");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            json_error_t error;
+            json_t *root = json_loads(chunk.memory, 0, &error);
+            if (root) {
+                json_t *response = json_object_get(root, "response");
+                if (json_is_string(response)) {
+                    printf("LLM Output: %s\n", json_string_value(response));
+                } else {
+                    printf("Response field not found or not a string.\n");
+                }
+                json_decref(root);
+            } else {
+                fprintf(stderr, "JSON parse error: %s\n", error.text);
+            }
+        }
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    }
+
+    free(chunk.memory);
+    curl_global_cleanup();
+    return 0;
 }
--- maple/ai_stub.h
+++ maple/ai_stub.h
@@ -35,19 +35,19 @@
 #define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#define PACKAGE "maple"
+#define PACKAGE "freecivai"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "maple"
+#define PACKAGE_NAME "FREECIVAI"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "maple 0.0.0"
+#define PACKAGE_STRING "FREECIVAI 0.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "maple"
+#define PACKAGE_TARNAME "freecivai"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
